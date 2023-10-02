25fd567ebd5c5803b8bc9cfef039c3b925dd0d01
micaelaferreyra2004 <117202573+micaelaferreyra2004@users.noreply.github.com>
2023-10-02 20:13:46 -0300

    Add files via upload

---
+++ Encuestas.jsx
@@ -0,0 +1,14 @@
+
+function Encuestas({ encuestas }) {
+return (
+<div>
+<h2>Encuestas Disponibles</h2>
+<ul>
+{encuestas.map(encuesta => (
+<li key={encuesta.id}>{encuesta.pregunta}</li>
+))}
+</ul>
+</div>
+);
+}
+export default Encuestas;
